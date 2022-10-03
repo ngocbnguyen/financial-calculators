ee91fb6d920a3ab2a1fee676a020e011cb8f6f95
Ngoc <ngocbnguyen19@gmail.com>
2022-10-03 10:11:28 -0400

    prettier format

--- scripts/morgage.js
+++ scripts/morgage.js
@@ -3,19 +3,19 @@
 window.onload = init;
 
 function init() {
-    const amountInput = document.querySelector("#amountInput").value;
-    const interestInput = document.querySelector("#interestInput").value;
-    const lengthInput = document.querySelector("#lengthInput").value;
-    const calculateBtn = document.querySelector("#calculateBtn").value;
-    calculateBtn.onclick = calculateBtnClicked;
+  const amountInput = document.querySelector("#amountInput").value;
+  const interestInput = document.querySelector("#interestInput").value;
+  const lengthInput = document.querySelector("#lengthInput").value;
+  const calculateBtn = document.querySelector("#calculateBtn").value;
+  calculateBtn.onclick = calculateBtnClicked;
 }
 
-
 function calculateBtnClicked() {
-    let paymentOutput = document.querySelector("#paymentOutput");
-    let interestOutput = document.querySelector("#interestOutput");
-    let payment = amountInput*(interestInput/(1-(1+interestInput)**-lengthInput));
-    let interest = (payment*interestInput)/12;
-    paymentOutput.value = payment;
-    interestOutput.value = interest;
+  let paymentOutput = document.querySelector("#paymentOutput");
+  let interestOutput = document.querySelector("#interestOutput");
+  let payment =
+    amountInput * (interestInput / (1 - (1 + interestInput) ** -lengthInput));
+  let interest = (payment * interestInput) / 12;
+  paymentOutput.value = payment;
+  interestOutput.value = interest;
 }
